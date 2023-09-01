6615e4e84631e3258eed9a2817b8ac601d25b8fb
lberthod <lberthod@gmail.com>
2023-09-01 10:25:14 +0200

    Update app.py
    a

--- app.py
+++ app.py
@@ -30,12 +30,18 @@ st.title('Décrivez votre cours')
 st.info("Dans cette zone de saisie, vous pouvez fournir des informations sur votre cours et votre matière pour aider QuizForge à générer un questionnaire précis pour vous.")
 prompt_title = st.text_input('Thématique de cours') 
 
+# Label pour le slider
+st.write("Nombre de questions")
+# Slider pour choisir un nombre entre 1 et 12
+numberQ = st.slider("Sélectionnez le nombre de questions", 1, 12, 1)
+
+
 
 
 # Prompt templates
 title_template = PromptTemplate(
-    input_variables = ['topic'], 
-    template = "En tant qu'expert en enseignement et en pédagogie, vous êtes chargé(e) de concevoir cinq questions à choix multiple pour évaluer la compréhension d'un sujet spécifique. Pour chaque question, veuillez fournir uniquement la question elle-même, sans les réponses ni les propositions de réponses. Voici la thématique : \n\n1. {topic}"
+    input_variables = ['prompt_title' , 'numberQ'], 
+    template = "En tant qu'expert en enseignement et en pédagogie, vous êtes chargé(e) de concevoir  {numberQ} questions à choix multiple pour évaluer la compréhension d'un sujet spécifique. Pour chaque question, veuillez fournir uniquement la question elle-même, sans les réponses ni les propositions de réponses. Voici la thématique : \n\n1. {prompt_title}"
 
 )
 
@@ -48,7 +54,7 @@ question_template = PromptTemplate(
 
 
 # Memory 
-title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+title_memory = ConversationBufferMemory(input_key='prompt_title', memory_key='chat_history')
 listq_memory = ConversationBufferMemory(input_key='list_question', memory_key='list_question_history')
 
 # Llms
@@ -60,24 +66,11 @@ question_chain = LLMChain(llm=llm, prompt=question_template, verbose=True, outpu
 # Show stuff to the screen if there's a prompt
 if st.button('Générer les questions'):
 
-    list_question = title_chain.run(prompt_title)
+    list_question = title_chain.run(prompt_title=prompt_title, numberQ = numberQ)
     st.info('Liste des questions : \n' +  list_question)
     number = 1
-    question = question_chain.run(list_question=list_question, number=number )
-    st.info('Question 1 : \n' +  question)
-
-    number = 2
-    question = question_chain.run(list_question=list_question, number=number )
-    st.info('Question 2 : \n' +  question)
-
-    number = 3
-    question = question_chain.run(list_question=list_question, number=number )
-    st.info('Question 3 : \n' +  question)
-
-    number = 4
-    question = question_chain.run(list_question=list_question, number=number )
-    st.info('Question 4 : \n' +  question)
-
-    number = 5
-    question = question_chain.run(list_question=list_question, number=number )
-    st.info('Question 5 : \n' +  question)
+   
+    for number in range(1, numberQ):
+        question = question_chain.run(list_question=list_question, number=number)
+        st.info(f'Question {number} : \n{question}')
+        number = number + 1
